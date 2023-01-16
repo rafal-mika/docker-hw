bb7fc3e05427e02a216157d7c4e18790e5bbceb6
Rafal Mika <rafal.mika@kaseya.com>
2023-01-16 14:03:07 +0100

    add task3 solution

---
+++ Task3-api-redis/README.md
@@ -0,0 +1,27 @@
+## Task 3 Docker - create Api and Redis via Docker-compose
+
+This is an API application that you can read and write entries to the Redis store.
+Entries are stored as key-value pairs.
+To deploy services, just run `docker-compose up` in source directory (docker-compose.yml location)
+
+-----
+### API description
+-----
+API base path: `http://<host>:5000/api`
+
+To get list of all keys go to `http://<host>:5000/api/keys` in your browser
+or create a request from CLI:
+
+- cUrl: `curl http://<host>:5000/api/keys`, 
+- Windows PowerShell: `Invoke-WebRequest -Uri http://<host>:5000/api/keys`
+
+To read an entry with a specified key:
+
+- browser: `http://<host>:5000/api/keys/<key>`
+- cUrl: `curl http://<host>:5000/api/keys/<key>`
+- Windows PowerShell: Invoke-WebRequest -Uri `http://<host>:5000/api/keys/<key>`
+
+To write an entry with a specified key:
+
+- cUrl: `curl -X POST http://<host>:5000/api/keys -H "Content-Type: application/json" -d '{"key":<key>, "value":<value>}'`
+- Windows PowerShell: `Invoke-WebRequest -Uri http://<host>:5000/api/keys -Method POST -ContentType "application/json" -Body '{"key":<key>, "value":<value>}'`
--- Task3-api-redis/api/api.py
+++ Task3-api-redis/api/api.py
@@ -1,7 +1,37 @@
-from fastapi import FastAPI
+import os
+from flask import Flask
+from flask import request
+from redis import Redis
+from flask_api import status
 
-app = FastAPI()
+app = Flask(__name__)
 
-@app.get("/")
-async def read_root():
-    return "hello world"
+redis_host = os.getenv("REDIS_HOST")
+redis_port = os.getenv("REDIS_PORT")
+
+redis = Redis(host=redis_host,port=redis_port,charset='utf-8',decode_responses=True)
+
+@app.get("/api/keys")
+def get_keys():
+    return {"keys":redis.keys('*')}
+
+@app.get("/api/keys/<key>")
+def get(key: str):
+    value = redis.get(key)
+    if value is None:
+        return f"Key '{key}' not found...", status.HTTP_404_NOT_FOUND
+    return {'key': key, 'value': value}
+
+@app.post("/api/keys")
+def create():
+    request_data = request.get_json()
+    key = request_data['key']
+    value = request_data['value']
+    if key is None or not bool(key.strip()) or value is None:
+        return "\n\nInvalid data format. Please check your entry data...", status.HTTP_400_BAD_REQUEST
+    
+    set_result = redis.set(key,value,nx=True)
+    return get(key) if set_result is not None else f"\n\nOperation was not performed. Key '{key}' already exists"
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0',port=5000)
